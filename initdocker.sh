f577584e9134ae5c3ba074217348f53c612d54ba
Mike Robertson <michael.w.robertson@gmail.com>
2016-08-25 13:21:06 -0400

    Updated initdocker.sh

--- initdocker.sh
+++ initdocker.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+echo "Configuring APT for Docker install..."
 sudo apt-key adv --keyserver --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+cp docker.list /etc/apt/sources.list.d/
+sudo apt-get update
+sudo apt-get purge lxc-docker
+sudo apt-cache policy docker-engine
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
+echo "Installing Docker..."
+sudo apt-get install docker-engine
+sudo service docker start
+
+echo "Configuring Docker..."
+sudo  groupadd docker
+sudo usermod -aG docker $USER
