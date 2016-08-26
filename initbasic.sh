f664721d480f38fa1aaaefa98437a233b60e835f
Mike Robertson <michael.w.robertson@gmail.com>
2016-08-26 13:18:18 -0400

    Updated initbasic.sh

--- initbasic.sh
+++ initbasic.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo "Updating applications..."
+echo -e "\e[1;42m Updating applications...\e[0m"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get clean
 sudo apt-get install apt-transport-https ca-certificates
+
+echo -e "\e[1;42m Copy common aliases...\e[0m"
+sudo cp .bash_aliases ~/.bash_aliases
